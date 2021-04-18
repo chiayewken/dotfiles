e68b682d1cdcaeeaa8d0298599eb46bc49011fc3
Chia Yew Ken <chiayewken@ken-macbook-2.local>
2021-04-18 19:23:03 +0800

    setup

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Dev Tools
+brew install tmux
+brew install node
+brew install universal-ctags
+brew install --cask miniconda
+brew install --cask hammerspoon
+brew install --cask karabiner-elements
+
+# Linters
+npm install -g pyright
+npm install -g prettier
